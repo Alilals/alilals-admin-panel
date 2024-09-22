883e7462f2d8192cd8beb653838732cf22d9e68d
Mumin Bashir Baba <mumin@pragtex.com>
2024-09-22 13:06:07 +0530

    added env file for firebase

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZSgfVFeipUZQuzqlEnr_F29jsyc5JXXQ",
-  authDomain: "test-project1-a8ea1.firebaseapp.com",
-  projectId: "test-project1-a8ea1",
-  storageBucket: "test-project1-a8ea1.appspot.com",
-  messagingSenderId: "236476283401",
-  appId: "1:236476283401:web:1a361c7442d86bbc85d6d8",
-  databaseURL: "https://test-project1-a8ea1-default-rtdb.firebaseio.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
+
 
 export const app = initializeApp(firebaseConfig);
