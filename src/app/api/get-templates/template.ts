ff11d8270bb50d25f2345c2b90538493aff53f69
tavkeer <shahtavkeer@gmail.com>
2025-06-26 19:03:39 +0530

    FEAT: template renamed

--- src/app/api/get-templates/template.ts
+++ src/app/api/get-templates/template.ts
@@ -3,7 +3,7 @@
 export interface TemplateEntry {
   template: string;
   'variable-count': number;
-  "message_id":string;
+  "template_id":string;
 }
 
 export interface TemplateMap {
@@ -16,7 +16,7 @@ const templates: TemplateMap = {
         "template":
         "Welcome to ALILAS  We're excited to have you onboard. At {#var#}, we strive to offer the best experience and support to help you get started. Whether you're here to explore, grow, or collaborate, we're here to guide you every step of the way.",
         'variable-count': 1,
-        "message_id":"188673",
+        "template_id":"188673",
       
     },
   ],
