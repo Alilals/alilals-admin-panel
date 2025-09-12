35e3a500e3d3f0dd8069ef17a35be000b0904355
tavkeer <shahtavkeer@gmail.com>
2025-09-12 14:47:55 +0530

    ✨ FEAT: Added new templetes

--- src/app/api/get-templates/template.ts
+++ src/app/api/get-templates/template.ts
@@ -32,7 +32,7 @@ const templates: TemplateMap = {
     },
     {
       template:
-        "We have received Rs. {#price#} for project ID {#project ID#}. Our representative will shortly connect with you for {#purpose#} .Team ZIRAAT®",
+        "We have received Rs. {#price#} for project ID community_id. Our representative will shortly connect with you for {#purpose#} .Team ZIRAAT®",
       "variable-count": 3,
       template_id: "191099",
       title: "Payment Confirmation",
@@ -57,6 +57,44 @@ const templates: TemplateMap = {
       "variable-count": 3,
       template_id: "194373",
       title: "Layout Team Deployment",
+    },
+
+
+
+    {
+      template:
+        "Dear Grower {#project_id#}! Your payment schedule has been updated with us as follows: First Instalment of Rs. {#first_instalment_amount#} due on {#first_instalment_date#}, Second Instalment of Rs. {#second_instalment_amount#} due on {#second_instalment_date#}, Third Instalment of Rs. {#third_instalment_amount#} due on {#third_instalment_date#}. Please make payments as scheduled to avoid any service delay. Team ZIRAAT® {#website_link#}.",
+      "variable-count": 8,
+      template_id: "197926",
+      title: "Payment Schedule",
+    },
+    {
+      template:
+        "Dear Grower {#project_id#}! Your Instalment amount of Rs. {#instalment_amount#} is due on {#due_date#}. Please pay on time to avoid service delays. Ignore if already paid. For assistance call our office at {#customer_care_number#}. Team ZIRAAT®",
+      "variable-count": 4,
+      template_id: "197923",
+      title: "Payment Due Date Reminder",
+    },
+    {
+      template:
+        "FINAL REMINDER! Your payment of Rs. {#overdue_amount#} is overdue. Please make the payment immediately to avoid any service disruption. Failure to clear the dues within 7 days will result in termination of enforceable service guarantees, including the Plant Mortality Replacement Guarantee. For assistance call our office at {#customer_care_number#}. Team ZIRAAT®",
+      "variable-count": 2,
+      template_id: "197924",
+      title: "Payment Overdue Reminder",
+    },
+    {
+      template:
+        "Dear Grower {#project_id#}! We have received payment of Rs. {#payment_amount#} on {#payment_date#}. Your Net outstanding Balance is {#ledger_balance#}. For any clarification, please call our Finance Department at {#finance_department_number#}. Team ZIRAAT®",
+      "variable-count": 5,
+      template_id: "197925",
+      title: "Payment Receipt with Ledger Balance",
+    },
+    {
+      template:
+        "Dear Grower {#project_id#}! Our {#service_type#} Team, headed by {#team_lead#} will visit the project site on {#service_date#}. Please remain available on the mentioned date. Team ZIRAAT®",
+      "variable-count": 4,
+      template_id: "197922",
+      title: "Service Team Deployment",
     },
   ],
 };
