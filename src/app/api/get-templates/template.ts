9aa5b58de893f4e80acef47d6e2e55810ecf1319
tavkeer <shahtavkeer@gmail.com>
2025-09-12 15:03:12 +0530

    ✨ FEAT: Build error fixed again

--- src/app/api/get-templates/template.ts
+++ src/app/api/get-templates/template.ts
@@ -32,7 +32,7 @@ const templates: TemplateMap = {
     },
     {
       template:
-        "We have received Rs. {#price#} for project ID community_id. Our representative will shortly connect with you for {#purpose#} .Team ZIRAAT®",
+        "We have received Rs. {#price#} for project ID {#project ID#}. Our representative will shortly connect with you for {#purpose#} .Team ZIRAAT®",
       "variable-count": 3,
       template_id: "191099",
       title: "Payment Confirmation",
@@ -53,7 +53,7 @@ const templates: TemplateMap = {
     },
     {
       template:
-        "Dear Grower!Your Site {#project ID#} has been assigned a Layout team. Team lead {#lead_name#}, they will visit the site on {#layout_date#}.Team ZIRAAT®",
+        "Dear Grower!Your Site {#project ID#} has been assigned a Layout team. Team lead {#lead name#}, they will visit the site on {#layout date#}.Team ZIRAAT®",
       "variable-count": 3,
       template_id: "194373",
       title: "Layout Team Deployment",
@@ -61,35 +61,35 @@ const templates: TemplateMap = {
 
     {
       template:
-        "Dear Grower {#project_id#}! Your payment schedule has been updated with us as follows: First Instalment of Rs. {#first_instalment_amount#} due on {#first_instalment_date#}, Second Instalment of Rs. {#second_instalment_amount#} due on {#second_instalment_date#}, Third Instalment of Rs. {#third_instalment_amount#} due on {#third_instalment_date#}. Please make payments as scheduled to avoid any service delay. Team ZIRAAT® {#website_link#}.",
+        "Dear Grower {#project ID#}! Your payment schedule has been updated with us as follows: First Instalment of Rs. {#first instalment amount#} due on {#first instalment date#}, Second Instalment of Rs. {#second instalment amount#} due on {#second instalment date#}, Third Instalment of Rs. {#third instalment amount#} due on {#third instalment date#}. Please make payments as scheduled to avoid any service delay. Team ZIRAAT® {#website link#}.",
       "variable-count": 8,
       template_id: "197926",
       title: "Payment Schedule",
     },
     {
       template:
-        "Dear Grower {#project_id#}! Your Instalment amount of Rs. {#instalment_amount#} is due on {#due_date#}. Please pay on time to avoid service delays. Ignore if already paid. For assistance call our office at {#customer_care_number#}. Team ZIRAAT®",
+        "Dear Grower {#project ID#}! Your Instalment amount of Rs. {#instalment amount#} is due on {#due date#}. Please pay on time to avoid service delays. Ignore if already paid. For assistance call our office at {#customer care number#}. Team ZIRAAT®",
       "variable-count": 4,
       template_id: "197923",
       title: "Payment Due Date Reminder",
     },
     {
       template:
-        "FINAL REMINDER! Your payment of Rs. {#overdue_amount#} is overdue. Please make the payment immediately to avoid any service disruption. Failure to clear the dues within 7 days will result in termination of enforceable service guarantees, including the Plant Mortality Replacement Guarantee. For assistance call our office at {#customer_care_number#}. Team ZIRAAT®",
+        "FINAL REMINDER! Your payment of Rs. {#overdue amount#} is overdue. Please make the payment immediately to avoid any service disruption. Failure to clear the dues within 7 days will result in termination of enforceable service guarantees, including the Plant Mortality Replacement Guarantee. For assistance call our office at {#customer care number#}. Team ZIRAAT®",
       "variable-count": 2,
       template_id: "197924",
       title: "Payment Overdue Reminder",
     },
     {
       template:
-        "Dear Grower {#project_id#}! We have received payment of Rs. {#payment_amount#} on {#payment_date#}. Your Net outstanding Balance is {#ledger_balance#}. For any clarification, please call our Finance Department at {#finance_department_number#}. Team ZIRAAT®",
+        "Dear Grower {#project ID#}! We have received payment of Rs. {#payment amount#} on {#payment date#}. Your Net outstanding Balance is {#ledger balance#}. For any clarification, please call our Finance Department at {#finance department number#}. Team ZIRAAT®",
       "variable-count": 5,
       template_id: "197925",
       title: "Payment Receipt with Ledger Balance",
     },
     {
       template:
-        "Dear Grower {#project_id#}! Our {#service_type#} Team, headed by {#team_lead#} will visit the project site on {#service_date#}. Please remain available on the mentioned date. Team ZIRAAT®",
+        "Dear Grower {#project ID#}! Our {#service type#} Team, headed by {#team lead#} will visit the project site on {#service date#}. Please remain available on the mentioned date. Team ZIRAAT®",
       "variable-count": 4,
       template_id: "197922",
       title: "Service Team Deployment",
